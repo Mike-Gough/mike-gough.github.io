69173a384b34e4595540c24004dd0eb419ede0b2
Mike <mikeyryan@me.com>
2020-08-09 17:22:10 +1000

    Update Publish version

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .executable(name: "MikeGoughMe", targets: ["MikeGoughMe"])
     ],
     dependencies: [
-        .package(url: "https://github.com/johnsundell/publish.git", from: "0.5.0"),
+        .package(url: "https://github.com/johnsundell/publish.git", from: "0.7.0"),
         .package(url: "https://github.com/SwiftyGuerrero/CNAMEPublishPlugin", from: "0.1.0"),
         .package(url: "https://github.com/wacumov/VerifyResourcesExistPublishPlugin", from: "0.1.0"),
         .package(url: "https://github.com/labradon/minifycsspublishplugin", from: "0.1.0"),
